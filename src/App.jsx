cfab06e7f0649cbfae6424e6544552cc140f4b97
Sai Myo Myat <saimyomyat511@gmail.com>
2022-06-12 06:15:35 +0630

    first push, nothing

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>welcome from blogs site</h1>
+      <button className="btn btn-primary">hi</button>
+    </div>
+  );
+}
+
+export default App;
